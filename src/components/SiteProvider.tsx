cdc31043c91384519d193f44f1ccc2022356365d
shan790903 <61260984+shan790903@users.noreply.github.com>
2025-07-28 11:25:56 +0800

    Update SiteProvider.tsx

--- src/components/SiteProvider.tsx
+++ src/components/SiteProvider.tsx
@@ -6,7 +6,7 @@ const SiteContext = createContext<{ siteName: string; announcement?: string }>({
   // 默认值
   siteName: 'MoonTV',
   announcement:
-    '本网站仅提供影视信息搜索服务，所有内容均来自第三方网站。本站不存储任何视频资源，不对任何内容的准确性、合法性、完整性负责。',
+    '本網站僅提供影視資訊搜尋服務，所有內容均來自第三方網站。本站不儲存任何影片資源，不對任何內容的準確性、合法性、完整性負責。',
 });
 
 export const useSite = () => useContext(SiteContext);
